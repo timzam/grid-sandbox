d697eadf28d436c2d4819a2bedcf968159785a3c
Timur Zambalayev <timur@jetbrains.com>
2021-10-12 17:36:47 -0400

    assets/css/grid-sandbox.css (initial)

--- src/main/kotlin/gridSandbox/process/GetPageResult.kt
+++ src/main/kotlin/gridSandbox/process/GetPageResult.kt
@@ -8,6 +8,8 @@ import kotlinx.html.dom.serialize
 import kotlinx.html.head
 import kotlinx.html.html
 import kotlinx.html.lang
+import kotlinx.html.link
+import kotlinx.html.meta
 import kotlinx.html.title
 
 fun getPageResult(pageTask: PageTask): PageOutput {
@@ -16,7 +18,14 @@ fun getPageResult(pageTask: PageTask): PageOutput {
         createHTMLDocument().html {
             lang = "en"
             head {
+                meta("viewport", "width=device-width, initial-scale=1")
                 title(title)
+                link("https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css", "stylesheet") {
+                    integrity = "sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+                    attributes["crossorigin"] = "anonymous"
+                    attributes["referrerpolicy"] = "no-referrer"
+                }
+                link("assets/css/grid-sandbox.css", "stylesheet")
             }
             body {
                 +bodyText
